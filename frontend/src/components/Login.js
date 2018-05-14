c1c19f8b3a3a7e37e70e52a31af25e58178a48da
phillips-jordan <jordan.phillips.nb@gmail.com>
2018-05-14 17:22:52 -0400

    getting started

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,14 +1,27 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import FacebookLogin from "react-facebook-login";
 
-//This Login Component will render the FB login 
+const responseFacebook = response => {
+  console.log(response);
+};
+
+//This Login Component will render the FB login
 
 class Login extends Component {
   render() {
-      return (<div>
-          LOGIN PAGE
-          </div>)
-
+    return (
+      <div>
+        LOGIN PAGE<br/>
+        <FacebookLogin
+          appId="132248777635494"
+          autoLoad={true}
+          fields="name,email,picture"
+        //  onClick={componentClicked}
+        //  callback={responseFacebook}
+        />
+      </div>
+    );
   }
 }
 

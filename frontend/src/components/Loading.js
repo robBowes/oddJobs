d9cfcd0e3bf47a6ea4fd394aed7b37b4696ec75c
phillips-jordan <jordan.phillips.nb@gmail.com>
2018-05-14 16:47:09 -0400

    more templating

---
+++ frontend/src/components/Loading.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+//This component is a loading screen to appear while
+//various components load
+
+class Loading extends Component {
+  render() {}
+}
+
+const mapStateToProps = state => ({
+  //redux props import
+});
+
+export default connect(mapStateToProps)(Loading);
---
+++ frontend/src/containers/LeftSideHide.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+//This component will exist outside of the frame on the
+//Left hand side. For Use with hiding components
+//and sliding animations to and from the left part
+//of screen
+
+class LeftSideHide extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default LeftSideHide;

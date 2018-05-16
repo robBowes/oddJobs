e2e6051b060576acb8bb4ef1a6e9be35bd9e465e
phillips-jordan <jordan.phillips.nb@gmail.com>
2018-05-16 11:44:52 -0400

    pulling

--- frontend/src/components/UserDetails.js
+++ frontend/src/components/UserDetails.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import {MoonLoader} from 'react-spinners'
 
 //This component renders the details for a given user
 //When viewing user details page
@@ -16,17 +17,18 @@ class UserDetails extends Component {
         this.setState({loading: false})
     }
   render() {
-      return this.state.loading===true?
-      <div>Loading data</div>
-        :
-      <div><img src={this.props.user.picture?this.props.user.picture.data.url:'loading image'}/>
-      <br/>
-      <h3>{this.props.user.name}</h3><br/>
-      {this.props.user.description}
-      <br/>
-      <br/>
-      Some rating 99
-      </div>
+      return this.state.loading === true ? <div>
+          <MoonLoader color="#05ff05" loading={this.state.loading} />
+        </div> : <div>
+          <img src={this.props.user.picture ? this.props.user.picture.data.url : "loading image"} />
+          <br />
+          <h3>{this.props.user.name}</h3>
+          <br />
+          {this.props.user.description}
+          <br />
+          <br />
+          Some rating 99
+        </div>;
   }
 }
 

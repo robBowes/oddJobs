d799e2b2f2a5531b1e4a973afe7c2ae79da46955
yazidmehenni <yazid129@gmail.com>
2018-05-16 14:36:51 -0400

    user setting page partially complete

--- frontend/src/components/UserSettings.js
+++ frontend/src/components/UserSettings.js
@@ -1,18 +1,125 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
-//This component renders the settings page and options for user
-//Users are able to see their current settings
-//and manipulate these settings via input boxes or sliders
-//show Image and Username
-//edit description, Categories, radius(slider) and min-max pay(input boxes)
-
 class UserSettings extends Component {
-  render() {}
+  constructor(props){
+    super(props)
+    this.state = {
+      description: '',
+      sliderValue: 50,
+      minPayValue: 0,
+      maxPayValue: 40,
+      categories: [],
+      boxCategories: ['general','lawncare','automotive','cleaning'
+      ,'plumbing','housekeeping','carpentry','pet care','it'],
+      loading: true,
+    }
+  }
+  handleSubmitButton = (event) => {
+
+  }
+  handleDescriptionChange = (event) => {
+   this.setState({ description: event.target.value });
+  }
+  sliderChange = event => {
+    this.setState({ sliderValue: event.target.value });
+  };
+  handleMinChange = event => {
+    event.preventDefault();
+    this.setState({ minPayValue: event.target.value });
+  };
+  handleMaxChange = event => {
+    event.preventDefault();
+    this.setState({ maxPayValue: event.target.value });
+  };
+  handleSubmit = event => {
+    event.preventDefault();
+  };
+  tickChange = event => {
+    if (event.target.checked){
+      let newCategories = [...this.state.categories];
+      newCategories = newCategories.concat(event.target.name);
+      this.setState({categories: newCategories})
+      return
+    }
+    this.setState({categories: this.state.categories.filter(x => x !== event.target.name)})
+  }
+  mapCheckBoxes = (categories) => {
+    return categories.map((x,i) => {
+    return (
+       <input onChange={this.tickChange} key ={i} className="categoryCheckBox" type="checkbox" name={x}/>
+  )
+    })
+  }
+  componentDidMount = () => {
+    this.setState({loading: false})
+  }
+  render() {
+    return this.state.loading?<div>Loading</div>:(
+      <div className="userSettingsPage">
+      <img className="userPicture" src={this.props.picture?this.props.picture.data.url:''} alt="user profile"/>
+      <form onSubmit={this.handleSubmit}> 
+       
+      <h2 className="settingsHeader"> Description: </h2>
+      <textarea
+        className="welcomeDescriptionInput"
+        value={this.state.description}
+        onChange={this.handleDescriptionChange}
+        type="textarea"
+        rows="5"
+        cols="50"
+        placeholder="This is who I am and the stuff I like to do!"
+      />
+          <h2 className="settingsHeader"> Show Jobs Within: </h2>
+          <input
+            type="range"
+            min="1"
+            max="100"
+            value={this.state.sliderValue}
+            onChange={this.sliderChange}
+            className="kmSlider"
+            id="welcomeSlider"
+          />
+          <h2 className="settingsHeader"> Show Jobs Paying: </h2>
+          <input
+            className="welcomeInputMinMax"
+            value={this.state.minPayValue}
+            onChange={this.handleMinChange}
+            type="number"
+            placeholder="from"
+          />{" "}
+          -{" "}
+          <input
+            className="welcomeInputMinMax"
+            value={this.state.maxPayValue}
+            onChange={this.handleMaxChange}
+            type="number"
+            placeholder="to"
+          />
+          <h2 className="welcomeHeader"> I'm interested in: </h2>
+            
+            {this.mapCheckBoxes(this.state.boxCategories)}
+
+          <button
+            type="submit"
+            className="welcomeButton"
+            onClick={this.handleSubmitButton}
+          >
+            Update
+          </button>    
+        </form>
+    </div>
+    )
+  }
 }
 
 const mapStateToProps = state => ({
-  //redux props import
+  picture: state.user.picture,
+  description: state.user.description,
+  maxDistance: state.user.maxDistance,
+  minPrice: state.user.minPrice,
+  maxPrice: state.user.maxPrice,
+  categories: state.props.categories,
 });
 
 export default connect(mapStateToProps)(UserSettings);
--- frontend/src/containers/Settings.js
+++ frontend/src/containers/Settings.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import UserSettings from './../components/UserSettings'
 
 class Settings extends Component {
     render() {
         return (
             <div>
-
+               <UserSettings/>
             </div>
         );
     }

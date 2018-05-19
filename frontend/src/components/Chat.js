2afcd7864d4aa32c561bcbc90ef04a7a65a364bc
phillips-jordan <jordan.phillips.nb@gmail.com>
2018-05-19 11:25:43 -0400

    deal stuff

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -38,17 +38,36 @@ class Chat extends Component {
         let chatFound = this.findChat(jobFound);
 
         let compare = window.location.href.split("/");
-        console.log(this.props);
         if (
           compare[compare.length - 1] === this.props.userid &&
           compare[compare.length - 2] === this.props.jobid &&
           compare[compare.length - 3] === "chats"
         ) {
+            console.log(this.props.user.id===jobFound.patronId)
+            let offerCheck = this.props.user.id === jobFound.patronId ? jobFound.dealsOfferedByHelpers.some(x => x === this.props.user.id) : 
+            jobFound.dealsOfferedByPatron.some(
+                    x => {
+                        
+                      return x === this.props.user.id;
+                    }
+                  );
+            let offerCheck2 = this.props.user.id !== jobFound.patronId ? jobFound.dealsOfferedByHelpers.some(x => x === this.props.user.id) : 
+            jobFound.dealsOfferedByPatron.some(
+                    x => {
+                      return x === this.props.userid;
+                    }
+                  );   
           this.setState({
             loading: false,
             messages: chatFound ? chatFound.messages : [],
-            job: jobFound
+            job: jobFound,
+            offered: offerCheck
+              ,
+            offer:
+              offerCheck2,
+            deal: jobFound.dealMade
           });
+          console.log(this.state)
           this.getAllMsgs();
         }
       });
@@ -56,8 +75,24 @@ class Chat extends Component {
 
   componentDidMount = () => {
     if (this.state.loading) {
+      this.getPartnerName();
       this.getAllMsgs();
     }
+  };
+
+  getPartnerName = () => {
+    fetch("/user", {
+      method: "POST",
+      credentials: "same-origin",
+      body: JSON.stringify({ id: this.props.userid })
+    })
+      .then(x => x.json())
+      .then(y => {
+        if (!y.status) {
+          throw new Error(y.reason);
+        }
+        this.setState({ name: this.props.user.name, partnerName: y.user.name });
+      });
   };
   componentWillReceiveProps = props => {
     this.setState({
@@ -80,7 +115,6 @@ class Chat extends Component {
       message: this.state.message,
       partner: this.state.partner
     };
-    console.log(x);
     fetch("/sendMessage", {
       method: "PUT",
       credentials: "same-origin",
@@ -93,12 +127,12 @@ class Chat extends Component {
           payload: y.user
         });
         let jobFound = this.findJob(y);
-        console.log(jobFound);
         let chatFound = this.findChat(jobFound);
-        console.log(chatFound);
         this.setState({ messages: chatFound.messages });
       })
-      .then(() => {});
+      .then(() => {
+        this.getAllMsgs();
+      });
     //this.setState({messages: messages})
     document.getElementById("chatbar").value = "";
   };
@@ -130,12 +164,83 @@ class Chat extends Component {
     return chat;
   };
 
+  deal = () => {
+    fetch("/deal", {
+      method: "PUT",
+      credentials: "same-origin",
+      body: JSON.stringify({
+        jobId: this.props.jobid,
+        counterParty: this.props.userid
+      })
+    })
+      .then(x => x.json())
+      .then(y => {
+        console.log(y);
+        if (y.status) {
+            y.job.dealMade ? this.setState({ deal: true }) : null;
+        if(this.props.user.id!==y.job.patronId){
+            console.log('test')
+          y.job.dealsOfferedByHelpers.some(x=>x===this.props.user.id)
+            ? this.setState({
+                offer: true
+              })
+            : null
+            this.sendDeal()
+        }
+          if (this.props.user.id === y.job.patronId) {
+            y.job.dealsOfferedByPatron.some(x => x === this.props.user.id) ? this.setState(
+                  {
+                    offer: true
+                  }
+                ) : null;
+                this.sendDeal()
+          }   
+
+        }
+      });
+  };
+
+  sendDeal=()=>{
+      let x = { id: this.state.job.id, message: 
+
+          this.state.offered?"I Accept the Deal! See you soon!":"I would like to offer a Deal! Click the Confirm button to accept the deal.",
+       partner: this.state.partner };
+      fetch("/sendMessage", {
+        method: "PUT",
+        credentials: "same-origin",
+        body: JSON.stringify(x)
+      })
+        .then(x => x.json())
+        .then(y => {
+          this.props.dispatch({
+            type: "MESSAGE_UPDATE",
+            payload: y.user
+          });
+          let jobFound = this.findJob(y);
+          let chatFound = this.findChat(jobFound);
+          this.setState({ messages: chatFound.messages });
+        })
+        .then(() => {
+          this.getAllMsgs();
+        });
+
+  }
+
   renderMessages = () => {
     if (this.state.messages.length > 0) {
       return this.state.messages.map((x, i) => {
         return (
-          <li id={x.id} key={i}>
-            {x.userId + ": " + x.message}
+          <li
+            className={
+              this.props.user.id === x.userId ? "userBubble" : "partnerBubble"
+            }
+            id={x.id}
+            key={i}
+          >
+            {
+              //(x.userId===this.props.user.id?this.state.name:this.state.partnerName) + ": " +
+              x.message
+            }
           </li>
         );
       });
@@ -148,8 +253,14 @@ class Chat extends Component {
       <div>
         <button onClick={this.goBack}>BACK</button>
         <button>HOME</button>
-        <div style={{ overflowY: "scroll" }} className="chatWindow">
-          <ul>{this.renderMessages()}</ul>
+        <div>
+          {this.state.partnerName + ` - "` + this.state.job.jobTitle + `"`}
+        </div>
+        <div
+          style={{ overflowY: "scroll", maxHeight: "82vh" }}
+          className="chatWindow"
+        >
+          <ul style={{ listStyleType: "none" }}>{this.renderMessages()}</ul>
         </div>
         <div
           style={{ bottom: "0vh", position: "absolute" }}
@@ -158,6 +269,11 @@ class Chat extends Component {
           <form onSubmit={this.handleSubmit}>
             <input type="text" onChange={this.handleChange} id="chatbar" />
           </form>
+          {this.state.deal?<button>DEAL MADE</button>:this.state.offer ? (
+            <button>DEAL SENT</button>
+          ) : (
+            <button onClick={this.deal}>{this.state.offered?'CONFIRM!':'SEND DEAL'}</button>
+          )}
         </div>
       </div>
     );

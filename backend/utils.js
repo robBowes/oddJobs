d4d0097924a40be4f9143ac846281144cd1c8767
robBowes <32183505+robBowes@users.noreply.github.com>
2018-05-21 19:44:50 -0400

    change mongo

--- backend/utils.js
+++ backend/utils.js
@@ -10,10 +10,10 @@ const makeNewUser = (user) => {
 };
 
 // const uri = 'mongodb+srv://user:1111@cluster0-b37en.mongodb.net/test?retryWrites=true';
-//  const uri = 'mongodb+srv://user:1111@cluster0-b37en.mongodb.net/test';
+ const uri = 'mongodb+srv://user:1111@cluster0-b37en.mongodb.net/test';
 
 // Jordans mongo server
-const uri = 'mongodb://user:1111@ds129670.mlab.com:29670/tester';
+// const uri = 'mongodb://user:1111@ds129670.mlab.com:29670/tester';
 
 const checkFbToken = async (fb) => {
     const response = await fetch('https://graph.facebook.com/v2.11/debug_token?input_token='+fb.accessToken+'&access_token=132248777635494|cf197eb7bd32b8dfab5cced1ee7e9a16');

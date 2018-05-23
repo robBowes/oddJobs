b746e8073e61ed618102929739557c02efa8a4df
phillips-jordan <jordan.phillips.nb@gmail.com>
2018-05-23 12:52:51 -0400

    job details page fix

--- frontend/src/components/JobDetails.js
+++ frontend/src/components/JobDetails.js
@@ -21,38 +21,52 @@ class JobDetails extends Component {
         this.props.dispatch({type: 'TOGGLE_LOADING'});
     }
     componentDidMount=()=>{
-        let job = this.props.jobs.filter((x) => {
-            console.log(x, this.props);
-            return x.id === this.props.id;
-          })[0];
-        fetch('/user', {
+        // let job = this.props.jobs.filter((x) => {
+        //     console.log(x, this.props);
+        //     return x.id === this.props.id;
+        //   })[0];
+        console.log(this.props.id)
+        fetch('/job',{
             method: 'POST',
             credentials: 'same-origin',
-            body: JSON.stringify({id: job.patronId}),
+            body: JSON.stringify({id: this.props.id})
         })
-        .then((x)=>x.json())
-        .then((y)=>{
-        if (!y.status) console.log(y);
-        this.setState({
-          loading: false,
-          job: job,
-          patron: y.user,
-        });
-        this.props.dispatch({type: 'TOGGLE_LOADING'});
-});
-    }
+        .then(x=>x.json())
+        .then(y=>{
+            console.log(y)
+            return y.job
+        })
+        .then(z=>{
+            let job = z
+            fetch('/user', {
+                method: 'POST',
+                credentials: 'same-origin',
+                body: JSON.stringify({id: job.patronId}),
+            })
+            .then((x)=>x.json())
+            .then((y)=>{
+                if (!y.status) console.log(y);
+                this.setState({
+                    loading: false,
+                    job: job,
+                    patron: y.user,
+                });
+                this.props.dispatch({type: 'TOGGLE_LOADING'});
+            });
+        })
+        }
   render() {
       return this.state.loading ? <div></div> :  <Animate to={"0.99"} from={"0.01"} attributeName="opacity" duration={1000}>
       <div>
             <div>
           <div className="pageTitle">
-            {this.props.jobs ? this.state.job.jobTitle + ' - ' + '$' + this.state.job.jobPay : null}
+            {this.state.job ? this.state.job.jobTitle + ' - ' + '$' + this.state.job.jobPay : null}
             </div>
             <div className='split'><hr/></div>
 
           </div>
           <div className="jobPictureContainer">
-            <img className="jobPicture" src={this.props.jobs ? this.state.job.picture : ''} alt="" />
+            <img className="jobPicture" src={this.state.job ? this.state.job.picture : ''} alt="" />
             <br />
           </div>
           <div className="patronBar">
@@ -62,7 +76,7 @@ class JobDetails extends Component {
           <div className="jobDescriptionDetailsWrapper">
 
           <div className='jobDetailsStyling'>Details:</div>
-          <span className='jobDetailDescription'>{this.props.jobs ? this.state.job.jobDescription : null}</span>
+          <span className='jobDetailDescription'>{this.state.job ? this.state.job.jobDescription : null}</span>
 
          
           </div>
